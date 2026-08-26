f908bc2bc48de9e681e015175ca5083875c52589
Vippy11-team <Walouma@hotmail.com>
2026-08-26 20:30:19 +0200

    Voeg nieuwsmodel en migration toe

---
+++ app/Models/NewsItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NewsItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'image',
+        'content',
+        'published_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'published_at' => 'date',
+        ];
+    }
+}
